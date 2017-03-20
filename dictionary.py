f1c51e80b25ae0640a50c777690650dbb2c2bc5a
dipesh <dipdreaming92@gmail.com>
2017-03-19 13:48:13 -1200

    dictionary.py

---
+++ dictionary.py
@@ -0,0 +1,10 @@
+user_info={
+	'name':'dipesh shrestha',
+	'email':'dip_dreaming92@gmail.com',
+	'address':'kathmandu,nepal'
+
+}
+
+print('name: %s' %user_info['name'])
+print('email: %s' %user_info['email'])
+print('address: %s' %user_info['address'])
