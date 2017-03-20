076ac092b715b59a6079dc2f6145e5874f2c3f9e
dipesh <dipdreaming92@gmail.com>
2017-03-19 13:38:24 -1200

    list.py

---
+++ list.py
@@ -0,0 +1,13 @@
+words=['this','is','just','a','test']
+capitalized_words=[x.capitalize() for x in words]
+print('words:',words)
+print('capital words:',capitalized_words)
+
+words= ['hello','world','foo','bar','test','python','is','awesome']
+short_words=[x for x in words if len(x) <= 3]
+other_words=[x for x in words if x not in short_words]
+words_with_e=[x for x in words if x.count('e') >=1]
+print('words:',words)
+print('short words:',short_words)
+print('other words:',other_words)
+print('words with e:',words_with_e)
